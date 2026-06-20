6ead4f8e7681ff24303452bb95931d7b588fa818
Dimitri-Blanchard <dimitriblanchard@proton.me>
2026-06-20 13:17:44 +0000

    deploy: frontend to docs/ [skip ci]

---
+++ docs/assets/index-CF1-Q9Sg.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-D9YJ3fYO.js","./index-DTrWB7JJ.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-BxLoIfUt.js";import{r}from"./index-DTrWB7JJ.js";import"./react-vendor-CYcp8n3f.js";import"./socket-vendor-DNoO1m3r.js";const o=r("Preferences",{web:()=>e(()=>import("./web-D9YJ3fYO.js"),__vite__mapDeps([0,1]),import.meta.url).then(e=>new e.PreferencesWeb)});export{o as Preferences};
